d8c3a3a4f1bc16b39f3b8d377d7674e3482588d1
Demkovychkate <Panchevakate1804@gmail.com>
2023-09-21 17:55:49 +0300

    Second Task

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let btn1= document.querySelector('.btnOne');
 let res1 = document.querySelector('.resOne');
 
+let btn2= document.querySelector('.btnTwo');
+let res2 = document.querySelector('.resTwo');
+
+
 //Task1
 function FirstTask(){
   let str1 = ' ';
@@ -11,3 +15,16 @@ function FirstTask(){
 }
 
 btn1.onclick=FirstTask;
+
+//Task2
+function SecondTask(){
+    let str2 = ' ';
+    for (let i = 10; i <= 100; i=i+10) {
+      str2 += (i*27) + ' ';
+      res2.innerHTML= str2;
+    }
+  }
+  
+  btn2.onclick=SecondTask;
+  
+  
